c41c5fb05ced0a1e055f4771904514b7f3e78b9a
Nolan Davidson <nolan.davidson@gmail.com>
2021-02-25 19:42:46 -0500

    adding initial impl

--- index.js
+++ index.js
@@ -2,5 +2,34 @@ const { Toolkit } = require('actions-toolkit')
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  tools.exit.success('We did it!')
+  require("action-guard")("pull_request.closed");
+  
+  const payload = tools.context.payload;
+
+  if (!payload.pull_request.merged) {
+    tools.log.warn("Pull request closed without merge");
+    return;
+  }
+
+  let pulls = await tools.github.paginate(
+    tools.github.pulls.list,
+    {
+      ...tools.context.repo,
+      state: "closed",
+      per_page: 100,
+    },
+    (response) => response.data
+  );
+
+  const expectedAuthor = payload.pull_request.user.login;
+  pulls = pulls.filter((p) => {
+    if (!p.merged_at) {
+      return false;
+    }
+
+    return p.user.login == expectedAuthor;
+  });
+
+  const pullCount = pulls.length;
+  tools.log.debug(`There are ${pullCount} Pull Requests`);
 })
